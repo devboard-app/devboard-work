cbcb6bcd7a272182dcf1aff5bd9341fb5b2c515e
Constantin Ilie <intern.constantin.ilie@assist.ro>
2026-08-17 11:53:45 +0300

    feat: linked views in urls

---
+++ projects/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import ProjectDetailView, ProjectListCreateView
+
+urlpatterns = [
+    path('', ProjectListCreateView.as_view()),
+    path('<uuid:project_id>', ProjectDetailView.as_view()),
+]
