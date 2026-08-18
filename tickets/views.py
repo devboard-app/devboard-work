498100f54a49a2f461d800927d3943b5e507502a
Constantin Ilie <intern.constantin.ilie@assist.ro>
2026-08-18 14:01:43 +0300

    feat: added views

--- tickets/views.py
+++ tickets/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+from projects.models import ProjectMembership
+from projects.services import get_project_or_404
+from teams.models import TeamMembership
+from work.views import AsyncAPIView
+
+from .permissions import require_project_role, require_team_role
+from .serializers import TicketListSerializer, TicketSerializer
+from .services import (
+    create_ticket,
+    delete_ticket,
+    get_ticket_or_404,
+    list_project_tickets,
+    update_ticket,
+)
+
+TeamRole = TeamMembership.Role
+ProjectRole = ProjectMembership.Role
+class TicketListCreateView(AsyncAPIView):
+
+    async def get(self, request, team_id, project_id):
+        await require_team_role(request.user.user_id, team_id, TeamRole.OWNER, TeamRole.ADMIN, TeamRole.MEMBER, TeamRole.VIEWER)
+        await require_project_role(request.user.user_id, project_id, ProjectRole.LEAD, ProjectRole.CONTRIBUTOR)
+        tickets = await list_project_tickets(project_id)
+        serializer = TicketListSerializer(tickets, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    async def post(self, request, team_id, project_id):
+        await require_team_role(request.user.user_id, team_id, TeamRole.OWNER, TeamRole.ADMIN, TeamRole.MEMBER, TeamRole.VIEWER)
+        membership = await require_project_role(request.user.user_id, project_id, ProjectRole.LEAD, ProjectRole.CONTRIBUTOR)
+        ticket = await create_ticket(await get_project_or_404(project_id), request.user.user_id, ProjectMembership.Role(membership.role), request.data)
+        serializer = TicketSerializer(ticket)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class TicketDetailView(AsyncAPIView):
+
+    async def get(self, request, team_id, project_id, ticket_id):
+        await require_team_role(request.user.user_id, team_id, TeamRole.OWNER, TeamRole.ADMIN, TeamRole.MEMBER, TeamRole.VIEWER)
+        await require_project_role(request.user.user_id, project_id, ProjectRole.LEAD, ProjectRole.CONTRIBUTOR)
+        ticket = await get_ticket_or_404(ticket_id)
+        serializer = TicketSerializer(ticket)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    async def patch(self, request, team_id, project_id, ticket_id):
+        await require_team_role(request.user.user_id, team_id, TeamRole.OWNER, TeamRole.ADMIN, TeamRole.MEMBER, TeamRole.VIEWER)
+        membership = await require_project_role(request.user.user_id, project_id, ProjectRole.LEAD, ProjectRole.CONTRIBUTOR)
+        ticket = await get_ticket_or_404(ticket_id)
+        updated_ticket = await update_ticket(ticket, request.user.user_id, ProjectMembership.Role(membership.role), request.data)
+        serializer = TicketSerializer(updated_ticket)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    async def delete(self, request, team_id, project_id, ticket_id):
+        await require_team_role(request.user.user_id, team_id, TeamRole.OWNER, TeamRole.ADMIN, TeamRole.MEMBER, TeamRole.VIEWER)
+        await require_project_role(request.user.user_id, project_id, ProjectRole.LEAD)
+        ticket = await get_ticket_or_404(ticket_id)
+        await delete_ticket(ticket)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
