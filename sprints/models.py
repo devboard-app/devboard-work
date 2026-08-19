a0c06c89fbba5c79fdb0f1af0553f0271529889c
Constantin Ilie <intern.constantin.ilie@assist.ro>
2026-08-19 08:53:23 +0300

    feat: created app and model + migration

---
+++ sprints/models.py
@@ -0,0 +1,23 @@
+import uuid
+
+from django.db import models
+
+
+class Sprint(models.Model):
+
+    class Status(models.TextChoices):
+        CREATED = 'created', 'Created'
+        ACTIVE = 'active', 'Active'
+        COMPLETED = 'completed', 'Completed'
+
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    name = models.CharField(max_length=100)
+    goal = models.TextField(blank=True)
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+    status = models.CharField(max_length=20, choices=Status.choices, default=Status.CREATED)
+    project = models.ForeignKey('projects.Project', on_delete=models.CASCADE, related_name='sprints')
+    created_by = models.UUIDField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
